330f25f69087d769738cab284bc009088035073d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-02-21 15:35:11 +0000

    Add organisation api service call

---
+++ src/infrastructure/organisation/organisationApiAccount.js
@@ -0,0 +1,48 @@
+const rp = require('request-promise');
+const jwtStrategy = require('login.dfe.jwt-strategies');
+const config = require('./../config');
+
+const callOrganisationApi = async (resource, body, method, reqId) => {
+  const token = await jwtStrategy(config.organisations.service).getBearerToken();
+  try {
+    const opts = {
+      method,
+      uri: `${config.organisations.service.url}/${resource}`,
+      headers: {
+        authorization: `bearer ${token}`,
+        'x-correlation-id': reqId,
+      },
+      json: true,
+    };
+    if (method === 'POST') {
+      opts.body = body;
+    }
+    const result = await rp(opts);
+
+    return {
+      success: true,
+      result,
+    };
+  } catch (e) {
+    return {
+      success: false,
+      statusCode: e.statusCode,
+      errorMessage: e.message,
+    };
+  }
+};
+
+const getServicesByUserId = async (id, reqId) => {
+  const response = await callOrganisationApi(`services/associated-with-user/${id}`, null, 'GET', reqId);
+  if (!response.success) {
+    if (response.statusCode === 404) {
+      return null;
+    }
+    throw new Error(response.errorMessage);
+  }
+  return response.result;
+};
+
+module.exports = {
+  getServicesByUserId,
+};
---
+++ src/infrastructure/organisation/staticOrganisation.js
@@ -0,0 +1,30 @@
+
+const orgServices = [
+  {
+    id: '3BFDE961-F061-4786-B618-618DEAF96E44',
+    name: 'Test Service (TS)',
+    description: 'A searchable test service.',
+    status: 1,
+    userId: '77D6B281-9F8D-4649-84B8-87FC42EEE71D',
+    requestDate: '2017-01-01T00:00:00.000Z',
+    approvers: [],
+    organisation: {
+      id: '88A1ED39-5A98-43DA-B66E-78E564EA72B0',
+      name: 'Test Org',
+    },
+    role: {
+      id: 0,
+      name: 'End user',
+    },
+    externalIdentifiers: [],
+  },
+];
+
+const getServicesByUserId = async (id) => {
+  const account = orgServices.find(item => item.userId.toLowerCase() === id.toLowerCase());
+  return Promise.resolve(account || null);
+};
+
+module.exports = {
+  getServicesByUserId,
+};
