ad6f7d533ec11dab092d38a4b6105ea45d5fd3f1
Floyd Price <f.price@floydprice.com>
2018-03-15 15:35:48 +0000

    increase keepalive sokcet pool to recomended default for iisnode

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ const { getErrorHandler } = require('login.dfe.express-error-handling');
 validateConfigAndQuitOnError(samlAssertionsApi, config, logger);
 
 http.GlobalAgent = new KeepAliveAgent({
-  maxSockets: 10,
-  maxFreeSockets: 2,
+  maxSockets: 160,
+  maxFreeSockets: 10,
   timeout: 60000,
   keepAliveTimeout: 300000,
 });
 https.GlobalAgent = new KeepAliveAgent({
-  maxSockets: 10,
-  maxFreeSockets: 2,
+  maxSockets: 160,
+  maxFreeSockets: 10,
   timeout: 60000,
   keepAliveTimeout: 300000,
 });
