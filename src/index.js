b8773b4510aea7071006e490d8883807a7c39b36
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-03-21 14:05:09 +0000

    Set keepalive through config

--- src/index.js
+++ src/index.js
@@ -17,16 +17,16 @@ const { getErrorHandler } = require('login.dfe.express-error-handling');
 validateConfigAndQuitOnError(samlAssertionsApi, config, logger);
 
 http.GlobalAgent = new KeepAliveAgent({
-  maxSockets: 160,
-  maxFreeSockets: 10,
-  timeout: 60000,
-  keepAliveTimeout: 300000,
+  maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+  maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+  timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+  keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
 });
 https.GlobalAgent = new KeepAliveAgent({
-  maxSockets: 160,
-  maxFreeSockets: 10,
-  timeout: 60000,
-  keepAliveTimeout: 300000,
+  maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+  maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+  timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+  keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
 });
 
 const app = express();
