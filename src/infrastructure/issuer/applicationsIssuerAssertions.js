881687a404083c4f2505f64649aa84ac432a2b08
Simon Bull <simon.bull.84@googlemail.com>
2018-10-24 14:28:40 +0100

    Create in-memory cache of assertion mappings
    Cache application responses for 5 minutes to reduce load

--- src/infrastructure/issuer/applicationsIssuerAssertions.js
+++ src/infrastructure/issuer/applicationsIssuerAssertions.js
@@ -1,6 +1,15 @@
 const { getServiceById } = require('./../applications');
 
-const getById = async (id, correlationId) => {
+let assertionsCache = [];
+
+const getServiceFromCacheOrApi = async (id, correlationId) => {
+  const cached = assertionsCache.find(x => x.id.toLowerCase() === id.toLowerCase());
+  if (cached && cached.expiresAt >= Date.now()) {
+    return cached.assertions;
+  } else if (cached) {
+    assertionsCache = assertionsCache.filter(x => x.id.toLowerCase() !== id.toLowerCase());
+  }
+
   const application = await getServiceById(id, correlationId);
   if (!application) {
     throw new Error(`Cannot get assertion mappings for service ${id} as it does not exist`, { correlationId });
@@ -9,12 +18,23 @@ const getById = async (id, correlationId) => {
     throw new Error(`Cannot get assertion mappings for service ${id} as it is not configured for SAML`, { correlationId });
   }
 
-  const assertions = (application.saml.assertions || []).map(a => ({
+  const assertions = application.saml.assertions || [];
+  assertionsCache.push({
+    id: id,
+    expiresAt: Date.now() + (5 * 60 * 1000), // Cache for 5 minutes
+    assertions,
+  });
+  return assertions;
+};
+
+const getById = async (id, correlationId) => {
+  const applicationAssertions = await getServiceFromCacheOrApi(id, correlationId);
+  const assertions = applicationAssertions.map(a => ({
     Type: a.type,
     Value: a.value,
     FriendlyName: a.friendlyName,
   }));
-  return {assertions};
+  return { assertions };
 };
 
 module.exports = {
