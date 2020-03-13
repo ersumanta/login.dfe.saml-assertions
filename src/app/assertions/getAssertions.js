ba48a54f992e1bd5217e8fb9e470671cf94c61b4
jovin-joy <jovin.joy@education.gov.uk>
2020-03-13 16:55:32 +0000

    NSA-2843-temp

--- src/app/assertions/getAssertions.js
+++ src/app/assertions/getAssertions.js
@@ -25,6 +25,7 @@ const get = async (req, res) => {
     if (!req.params.userId || !req.params.serviceId) {
       return res.status(400).send();
     }
+    logger.info('Corona Virus Test::req.params.serviceId:'+req.params.serviceId);
 
     const user = await accountApi.getById(req.params.userId, correlationId);
     if (!user) {
@@ -42,39 +43,62 @@ const get = async (req, res) => {
     }
 
     const services = await getServicesByUserId(req.params.userId, correlationId);
-    if (!services) {
-      logger.info('Corona Virus Test:: !services');
-      return res.status(404).send();
+    if(req.params.serviceId === 'b45616a1-19a7-4a2e-966d-9e28c99bc6c6') {
+      const organisation = await organisationApi.getOrganisationById(req.params.organisationId, correlationId);
+      if (!organisation) {
+        logger.info('Corona Virus Test:: !getOrganisationById');
+        return res.status(404).send();
+      }
+
+      const issuerAssertion = await issuerAssertions.getById(req.params.serviceId);
+      if (!issuerAssertion) {
+        logger.info('Corona Virus Test:: !issuerAssertion');
+        return res.status(404).send();
+      }
+      const userAccountAssertionModel = new UserAccountAssertionModel()
+          .setUserPropertiesFromAccount(user)
+          .setUserPropertiesFromUserOrganisation(userOrganisation)
+          .setServicePropertiesFromService(null)
+          .setOrganisationPropertiesFromOrganisation(organisation)
+          .buildAssertions(issuerAssertion.assertions);
+
+      const result = userAccountAssertionModel.export();
+      res.send(result);
+    }else {
+      if (!services) {
+        logger.info('Corona Virus Test:: !services');
+        return res.status(404).send();
+      }
+
+      const servicesMeetingCriteria = services.filter(s => doesServiceMeetRequestCriteria(s, req));
+      const service = servicesMeetingCriteria && servicesMeetingCriteria.length > 0 ? servicesMeetingCriteria[0] : undefined;
+      if (!service) {
+        logger.info('Corona Virus Test:: !servicesMeetingCriteria');
+        return res.status(404).send();
+      }
+
+      const organisation = await organisationApi.getOrganisationById(service.organisationId, correlationId);
+      if (!organisation) {
+        logger.info('Corona Virus Test:: !getOrganisationById');
+        return res.status(404).send();
+      }
+
+      const issuerAssertion = await issuerAssertions.getById(service.serviceId);
+      if (!issuerAssertion) {
+        logger.info('Corona Virus Test:: !issuerAssertion');
+        return res.status(404).send();
+      }
+
+      const userAccountAssertionModel = new UserAccountAssertionModel()
+          .setUserPropertiesFromAccount(user)
+          .setUserPropertiesFromUserOrganisation(userOrganisation)
+          .setServicePropertiesFromService(service)
+          .setOrganisationPropertiesFromOrganisation(organisation)
+          .buildAssertions(issuerAssertion.assertions);
+
+      const result = userAccountAssertionModel.export();
+      res.send(result);
     }
-
-    const servicesMeetingCriteria = services.filter(s => doesServiceMeetRequestCriteria(s, req));
-    const service = servicesMeetingCriteria && servicesMeetingCriteria.length > 0 ? servicesMeetingCriteria[0] : undefined;
-    if (!service) {
-      logger.info('Corona Virus Test:: !servicesMeetingCriteria');
-      return res.status(404).send();
-    }
-
-    const organisation = await organisationApi.getOrganisationById(service.organisationId, correlationId);
-    if (!organisation) {
-      logger.info('Corona Virus Test:: !getOrganisationById');
-      return res.status(404).send();
-    }
-
-    const issuerAssertion = await issuerAssertions.getById(service.serviceId);
-    if (!issuerAssertion) {
-      logger.info('Corona Virus Test:: !issuerAssertion');
-      return res.status(404).send();
-    }
-
-    const userAccountAssertionModel = new UserAccountAssertionModel()
-      .setUserPropertiesFromAccount(user)
-      .setUserPropertiesFromUserOrganisation(userOrganisation)
-      .setServicePropertiesFromService(service)
-      .setOrganisationPropertiesFromOrganisation(organisation)
-      .buildAssertions(issuerAssertion.assertions);
-
-    const result = userAccountAssertionModel.export();
-    res.send(result);
   } catch (e) {
     logger.error(e);
     res.status(500).send();
