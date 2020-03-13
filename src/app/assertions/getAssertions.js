2047579d963edabd640a88a3a13f9b1eb96eca52
jovin-joy <jovin.joy@education.gov.uk>
2020-03-13 17:33:21 +0000

    NSA-2843-temp

--- src/app/assertions/getAssertions.js
+++ src/app/assertions/getAssertions.js
@@ -6,6 +6,7 @@ const organisationApi = require('./../../infrastructure/organisation');
 const UserAccountAssertionModel = require('./userAssertionModel');
 const issuerAssertions = require('./../../infrastructure/issuer');
 const { getServicesByUserId } = require('./../../infrastructure/access');
+const { getServiceById } = require('./../../infrastructure/applications');
 const doesServiceMeetRequestCriteria = (service, req) => {
   if (service.serviceId.toLowerCase() !== req.params.serviceId.toLowerCase()) {
     return false;
@@ -36,7 +37,6 @@ const get = async (req, res) => {
     if (req.params.organisationId) {
       const userOrganisations = await organisationApi.getOrganisationsForUser(req.params.userId, correlationId) || [];
       userOrganisation = userOrganisations.find(x => x.organisation.id.toLowerCase() === req.params.organisationId.toLowerCase());
-      logger.info('Corona Virus Test::'+userOrganisation);
       if (!userOrganisation) {
         return res.status(404).send();
       }
@@ -49,19 +49,24 @@ const get = async (req, res) => {
         logger.info('Corona Virus Test:: !getOrganisationById');
         return res.status(404).send();
       }
-
       const issuerAssertion = await issuerAssertions.getById(req.params.serviceId);
       if (!issuerAssertion) {
         logger.info('Corona Virus Test:: !issuerAssertion');
         return res.status(404).send();
       }
+      const service = await getServiceById(req.params.serviceId,correlationId);
+      if (!service) {
+        logger.info('Corona Virus Test:: !serviceCriteria');
+        return res.status(404).send();
+      }
+      logger.info('Corona Virus Test:: service here..service'+service);
       const userAccountAssertionModel = new UserAccountAssertionModel()
           .setUserPropertiesFromAccount(user)
           .setUserPropertiesFromUserOrganisation(userOrganisation)
-          .setServicePropertiesFromService(null)
+          .setServicePropertiesFromService(service)
           .setOrganisationPropertiesFromOrganisation(organisation)
           .buildAssertions(issuerAssertion.assertions);
-
+      logger.info('Corona Virus Test:: Ends here..');
       const result = userAccountAssertionModel.export();
       res.send(result);
     }else {
